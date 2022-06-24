0bde371f4d56e54a2445fcf8c79a08d254c8c542
Ricardo Flores <rik.fm1809@gmail.com>
2022-06-24 14:13:25 -0500

    ejercicio de la clases 4,5,6

--- src/exercises456/Clock.tsx
+++ src/exercises456/Clock.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useRef, useState } from "react"
 
+interface Person {
+   nombre: string
+   apellidos: string
+   edad: number
+   fecha: Date
+}
 export const Clock = () => {
+   const [person, setPerson] = useState<Person>({
+      nombre: 'Martin',
+      apellidos: 'San Jose',
+      edad: 0,
+      fecha: new Date()
+   })
+   const tick = () => {
+      setPerson(prevSate => ({
+         ...prevSate,
+         edad: prevSate.edad + 1,
+         fecha: new Date()
+      } as Person ))
+   }
+   useEffect(() => {
+      const timeID = setInterval(
+         () => tick(), 1000
+      )
+     return () => {
+      clearInterval(timeID)
+      console.log( 'timer desmontado!!' )
+     }
+   }, [])
+   
    return (
-      <div>
-         Clock
+      <div className="mt-5 p-4 border shadow rounded-lg">
+         <h2><span className="text-sky-600">Hora Actual:</span> {person.fecha.toLocaleTimeString()}</h2>
+         <h3><span  className="text-sky-600">Usuario: </span>
+             {person.nombre} {person.apellidos}
+         </h3>
+         <h1><span  className="text-sky-600"> Edad:</span> {person.edad}</h1>
       </div>
-   )
+   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,37 +1,52 @@
-import { ContactList } from "../components/contact/ContactList"
-import { Header } from "../components/header/Header"
-import { TaskList } from "../components/TaskList"
-import { ComponentA } from "../ejercicios123/ComponentA"
-import { Clock } from "../exercises456/Clock"
-import { Task } from "../interfaces/Task"
-import { MiComponenteConContexto } from "../lesson456/UseContext"
+import { useState } from 'react';
+import { ContactList } from '../components/contact/ContactList';
+import { Header } from '../components/header/Header';
+import { TaskList } from '../components/TaskList';
+import { ComponentA } from '../ejercicios123/ComponentA';
+import { Clock } from '../exercises456/Clock';
+import { Task } from '../interfaces/Task';
+import { MiComponenteConContexto } from '../lesson456/UseContext';
 
-const listaTareas:Task[] = [
+const listaTareas: Task[] = [
    {
       id: '1',
       name: 'task 1',
       desc: 'abrir el canio',
-      level: "normal",
-      completed: false
+      level: 'normal',
+      completed: false,
    },
    {
       id: '2',
       name: 'task 2',
       desc: 'dormir bien',
-      level: "normal",
-      completed: false
+      level: 'normal',
+      completed: false,
    },
-]
+];
 export const HomePage = () => {
+   const [isMounted, setIsMounted] = useState(true);
+   const handleClick = () => {
+      setIsMounted(!isMounted);
+   };
    return (
       <div className="bg-slate-50 h-screen">
          <main>
             <div className="flex w-auto place-content-center">
                {/* <ComponentA /> */}
                {/* <MiComponenteConContexto /> */}
-               <Clock />
+
+               {isMounted && <Clock />}
+               
+               <div className="mt-5 ml-2">
+                  <button
+                     className="p-2 rounded-md border shadow-md text-orange-500 shadow-orange-300 hover:shadow-none hover:bg-orange-300 hover:text-slate-100 transition-all"
+                     onClick={handleClick}
+                  >
+                     desmontar
+                  </button>
+               </div>
             </div>
          </main>
       </div>
-   )
-}
+   );
+};
